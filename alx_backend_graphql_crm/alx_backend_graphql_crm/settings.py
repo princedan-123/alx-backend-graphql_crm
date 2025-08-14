641d0f6a7f451c23d5b64b890ec749cb60579fcc
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-08-14 11:11:53 +0100

    added graphene schema config

--- alx_backend_graphql_crm/alx_backend_graphql_crm/settings.py
+++ alx_backend_graphql_crm/alx_backend_graphql_crm/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+GRAPHENE = {
+    "SCHEMA": "graphql_project.schema.schema"  # path to schema object
+}
