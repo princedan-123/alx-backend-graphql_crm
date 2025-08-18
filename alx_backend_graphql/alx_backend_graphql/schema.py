2ee35090f1fe19593e3c9e7aac269a2298e23f12
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2025-08-18 15:00:44 +0100

    changed directory structure to test checker

--- alx_backend_graphql/alx_backend_graphql/schema.py
+++
@@ -1,9 +0,0 @@
-import graphene
-
-class Query(graphene.ObjectType):
-    hello = graphene.String()
-
-    def resolve_hello(self, info, *args, **kwargs):
-        return "Hello, GraphQL!"
-
-schema = graphene.Schema(query=Query)
